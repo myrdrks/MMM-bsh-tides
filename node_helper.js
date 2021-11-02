6a9d14759713119b882f0e1392583338e46551e0
Philip <philip.meyerdierks@gmail.com>
2021-11-02 10:52:55 +0100

    improved filter function

--- node_helper.js
+++ node_helper.js
@@ -4,7 +4,6 @@ const moment = require('moment');
 const NodeHelper = require('node_helper');
 
 module.exports = NodeHelper.create({
-
   socketNotificationReceived: function(notification, payload) {
     if(notification === 'START'){
         this.config = payload;
@@ -14,9 +13,7 @@ module.exports = NodeHelper.create({
         }, this.config.updateInterval);
     }
   },
-
   readData: function(){
-    //to read a file to do the following
     const l = {
       N: this.config.lowTide,
       H: this.config.highTide,
@@ -25,18 +22,23 @@ module.exports = NodeHelper.create({
     const fileContent = buffer.toString();
     const elements = fileContent.split('\n');
 
-    const getTides = (d) => elements.filter(el => el.replace(/ /g,"").includes(d.format("D.M.YYYY"))).map(el => {
+    const formattedElements = elements.filter(el => el.split('#')[0] === 'VB2').map(el => {
       const e = el.split('#');
       const tide = l[e[3]];
-      const zeit = moment(e[6],'H:mm').add(e[10].split(":")[0].replace(/\D+/g, ''), 'hour').format("HH:mm");
-      const datum = e[5];
-      if(tide && zeit)
-        return { date: datum, tide, time: zeit };
-        // return `${datum} ${tide} um ${zeit} Uhr`;
+      const timestr = `${moment(e[5] && e[5].replace(/ /g, '0'), 'DD.MM.YYYY').format('YYYY-MM-DD')} ${e[6]} ${e[10].replace(/ /g, 0)}`;
+      const datetime = moment(timestr, "YYYY-MM-DD HH:mm Z");
+      
+      return ({
+        tide,
+        datetime,
+        timestr,
+      })
     });
+
+    const getTides = (d) => formattedElements.filter(f => f.datetime.format('DDMMYYY') === d.format('DDMMYYY'));
+
     const today = getTides(moment());
     const tomorrow = getTides(moment().add(1,'days'));
     this.sendSocketNotification('TIDEDATA', { today, tomorrow });
-
   }
 });
